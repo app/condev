1b25389deb0ee97eea11614b4db5409fd9ba6756
Andrey Paskal <apaskal@gmail.com>
2022-05-16 10:42:53 +0300

    This commit adds user and context switching

--- start.sh
+++ start.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 # Stop on errors
 set -e
+# Default user is node
+[ -n "$USERID" ] || USERID=`id -u node`
+
 # Switch to user with same UID as host user; Create user if needed
-if ( id -un "$USERID" >/dev/null 2>&1 ); then
-  UNAME =`id -un "$USERID"`
+if ( grep ":$USERID:" /etc/passwd > /dev/null  ); then
+  UNAME=` grep ":$USERID:" /etc/passwd| awk -F ':' '{print $1}'`
 else
-  adduser --uid $USERID --gid $USERID --disabled-password --gecos "" condev
+  UNAME=condev
+  adduser --uid $USERID --disabled-password --gecos "" $UNAME
 fi
-su $UNAME -
+
+# Default values
+[ -n "$TERM" ] || TERM="xterm-256color"
+[ -n "$GIT_COMMITTER_NAME" ] || GIT_COMMITTER_NAME="app"
+[ -n "$GIT_AUTHOR_NAME" ] || GIT_AUTHOR_NAME="Andrey Paskal"
+[ -n "$EMAIL" ] || EMAIL="andrey@paskal.email"
+[ -n "$D" ] || D="D1"
+[ -n "$DD" ] || DD="DD1"
+
+VARS="\
+  TERM=\"$TERM\" \
+  GIT_COMMITTER_NAME=\"${GIT_COMMITTER_NAME}\" \
+  GIT_AUTHOR_NAME=\"${GIT_AUTHOR_NAME}\" \
+  EMAIL=\"$EMAIL\" \
+  USERID=\"$USERID\" \
+  SSH_AUTH_SOCK=\"$SSH_AUTH_SOCK\" \
+"
 
 if [ $# -gt 0 ]; then
-  exec "$@"
+  su -s /bin/bash -c "$VARS $@" - $UNAME
 else
-  exec "tmux -u new-session -s condev"
+  su -s /bin/bash -c "$VARS tmux -u new-session -s condev" - $UNAME
 fi
 
